7272af1c46a1cb20d176350b2f39aa3cf0da92b4
Wojciech Lamperski <wolamp20@gmail.com>
2023-06-12 18:29:17 +0200

    adding content to pages

--- components/GlassPane.tsx
+++ components/GlassPane.tsx
@@ -1,6 +1,6 @@
 import clsx from "clsx";
 
-const GlassPane = ({ children }) => {
+const GlassPane = ({ children, className }) => {
   return (
     <div
       className={clsx(
@@ -12,3 +12,5 @@ const GlassPane = ({ children }) => {
     </div>
   );
 };
+
+export default GlassPane;
